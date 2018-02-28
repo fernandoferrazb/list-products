51858815e57054c075644597736038c485006560
fernandoferrazb <fernandoferrazbarreto@gmail.com>
2018-02-28 13:06:59 -0300

    Server mocks JSON

--- app/app.js
+++ app/app.js
@@ -10,12 +10,21 @@ app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', __dirname + ' /../views');
 
-app.get('/*/', function (req, res) {
+app.get('/', function (req, res) {
 	console.log(req.url);
 	res.render('index', {
 		title: 'List products Init'
 	});
 });
 
+//Server static JSON
+var path = require("path");
+app.get('/server', function (req, res) {
+	res.sendFile(path.normalize(__dirname + '/../mocks/server.json'));
+});
+
 appServer = app.listen(process.env.PORT || 8000);
 console.log("Express server started");
+
+
+
